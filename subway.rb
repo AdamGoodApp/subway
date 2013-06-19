275da959f385f6a5d250f42e8fef011a60aeadaf
theponti <cjponti@gmail.com>
2013-06-19 23:33:53 +0100

    Fully Functional

--- subway.rb
+++ subway.rb
@@ -1,81 +1,85 @@
-
+#----------  Train Lines ----------#
 L = ['8th ave', '6th ave','Union square','3rd ave','1st ave']
-N =['8th st','6th st','Union square','West 23rd','West 28th','West 34th','Times square']
-SIX =['Astor place','Union square','East 23rd','East 28th','East 33rd','Grand central']
+N =['8th st','Union square','West 23rd','West 28th','West 34th','Times square']
+S =['Astor place','Union square','East 23rd','East 28th','East 33rd','Grand central']
 
+#----------  Helper Functions ----------#
+def gett() gets.chomp.downcase end
 
-puts `clear`
-print "**********Subway Mappr**********\n"
-
-def gett
-  gets.chomp.downcase
-end
+def titlr(title) print ("-" * 10) + " #{title} " + ("-" * 10)  end
 
 def check_input(array, input)
-   if !(array.include? input)
-     p "That is not a valid station"
-   end
+  p "That is not a valid station" if !(array.include? input)
 end
 
 def print_stations(line)
-  line.each do |station|
-    print "\n#{station}   "
-  end
+  line.each { |s| print "\n#{s}" }
   puts
 end
 
+def distance(x_index, y_index)
+  x_index > y_index ? x_index - y_index : y_index - x_index
+end
+
+def assign_train(line)
+ case line
+  when 'l' then L
+  when 'n' then N
+  when '6' then S
+ end
+end
+
+def get_train(line)
+  case line
+    when L then  'L'
+    when N then  'N'
+    when S then  '6'
+  end
+end
+
+def plural(line)
+   line > 1 ? "#{line} stops" : "#{line} stop"
+end
+
+#----------  Application ----------#
+puts `clear`
+puts titlr('Subway Mappr')
+puts " I'm going to help you find your way! "
 
 print "\nWhich line are you on?  (L) (N) (6)  "
-first_line = gett
+line1 = assign_train(gett)
 
 print "\nWhich station are you starting from?  "
-case first_line
-  when 'l'
-    first_line = L
-    print_stations L
-  when 'n'
-    first_line = N
-    print_stations N
-  when '6'
-    first_line = SIX
-    print_stations SIX
-end
+print_stations line1
 puts
-first_station = gett.capitalize
-check_input(first_line, first_station)
+start = gett.capitalize
+check_input(line1, start)
 
 puts `clear`
 print "\nWhich line would you like to get off at? (L) (N) (6)  "
-second_line = gett
+line2 = assign_train(gett)
 
 print "\nWhich station would you like to get off at?  "
-case second_line
-  when 'l'
-    second_line = L
-    print_stations L
-  when 'n'
-    second_line = N
-    print_stations N
-  when '6'
-    second_line = SIX
-    print_stations SIX
-end
+print_stations line2
 puts
 stop = gett.capitalize
-check_input(second_line, stop)
+check_input(line2, stop)
 
-first_index = first_line.index(first_station)
-stop_index = second_line.index(stop)
-union_index1 = first_line.index('Union square')
-union_index2 = second_line.index('Union square')
+puts `clear`
+print ("-" * 10) + " Your directions " + ("-" * 10)
 
-if first_line == second_line
-  d = stop_index - first_index
-  p "You have to travel #{d} stops to #{stop}."
+if line1 == line2
+  d = line2.index(stop) - line1.index(start)
+  p "You have to travel #{d} stops on this train to #{stop}."
 else
-  d = first_index > union_index1 ? first_index - union_index1 : union_index1 - first_index
-  d2 = first_index > union_index1 ? stop_index - union_index2 : union_index2 - stop_index
-  p "You have to travel #{d} stops to Union Square, transfer to the #{second_line}
-      line, and take it #{d2} stops to #{stop}"
+  d = distance( line1.index(start), line1.index('Union square') )
+  d2 = distance( line2.index(stop), line2.index('Union square') )
+  puts "\n(1) Travel #{plural(d)} to Union Square."
+  puts "(2) Transfer to the #{get_train(line2)} line."
+  puts "(3) Take it #{plural(d2)} to #{stop}.\n"
 end
+
+puts "\nHave a pleasant trip!"
+gets
+
 
